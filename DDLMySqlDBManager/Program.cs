b97a48159702bc4539c19a11e0fb18e97c9d6787
unknown <rushikeshchikane101@gmail.com>
2022-12-07 18:51:40 +0530

    dotnet Db Connectivity program

---
+++ DDLMySqlDBManager/Program.cs
@@ -0,0 +1,26 @@
+﻿using DAL;
+
+
+
+// Creating table
+//MySQLDBManager.CreateTable();
+
+
+
+// Inserting data into table
+//MySQLDBManager.Insert("Akash","Ajab","M.Tech");
+//MySQLDBManager.Insert("Akshay","Tanpure","B.COM");
+//MySQLDBManager.Insert("Rohit","Gore","B.Tech");
+
+
+//Updating into students
+//MySQLDBManager.Update(1,"Rushikesh ","Chikane "," BE");
+
+//deleting from students
+//MySQLDBManager.Delete(6);
+
+//Showing studentslist from students
+//MySQLDBManager.ShowAllStudents();
+
+//show student by id
+MySQLDBManager.ShowStudentById(1);
